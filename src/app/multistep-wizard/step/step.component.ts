1c583f154ad5c9ba098b09f2201752e3cec14a1c
William Cheung <william.cheung.c@gmail.com>
2023-07-15 00:49:17 +0100

    added animations to StepComponent

--- src/app/multistep-wizard/step/step.component.ts
+++ src/app/multistep-wizard/step/step.component.ts
@@ -1,6 +1,22 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+  OnChanges,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckIconComponent } from '../check-icon/check-icon.component';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+
+type StepStatus = 'inactive' | 'active' | 'complete';
 
 @Component({
   selector: 'app-step',
@@ -9,28 +25,69 @@ import { CheckIconComponent } from '../check-icon/check-icon.component';
   templateUrl: './step.component.html',
   styleUrls: ['./step.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('colours', [
+      state(
+        'inactive',
+        style({
+          backgroundColor: 'var(--color-white)',
+          borderColor: 'var(--color-slate-200)',
+          color: 'var(--color-slate-400)',
+        })
+      ),
+      state(
+        'active',
+        style({
+          backgroundColor: 'var(--color-white)',
+          borderColor: 'var(--color-blue-500)',
+          color: 'var(--color-blue-500)',
+        })
+      ),
+      state(
+        'complete',
+        style({
+          backgroundColor: 'var(--color-blue-500)',
+          borderColor: 'var(--color-blue-500)',
+          color: 'var(--color-blue-500)',
+        })
+      ),
+      transition('* => *', [animate('0.2s')]),
+    ]),
+
+    trigger('glow', [
+      state('active', style({ scale: 1 })),
+      state('complete', style({ scale: 1.3 })),
+      transition('active => complete', [
+        /** Ease-out-circ @see @link https://easings.net/#easeOutCirc */
+        animate('0.6s 0.2s cubic-bezier(0, 0.55, 0.45, 1)'),
+      ]),
+      transition('complete => active', [animate('0.2s')]),
+    ]),
+  ],
 })
-export class StepComponent {
+export class StepComponent implements OnChanges, AfterViewInit {
   @Input({ required: true }) step!: number;
   @Input({ required: true }) currentStep!: number;
 
-  private _status = 'inactive';
+  @HostBinding('@.disabled') animationsDisabled = true;
+
+  status: StepStatus = 'inactive';
 
   ngOnChanges() {
     this.updateStatus();
   }
 
-  get status() {
-    return this._status;
+  ngAfterViewInit() {
+    this.animationsDisabled = false;
   }
 
   private updateStatus() {
     if (this.currentStep > this.step) {
-      this._status = 'complete';
+      this.status = 'complete';
     } else if (this.currentStep === this.step) {
-      this._status = 'active';
+      this.status = 'active';
     } else {
-      this._status = 'inactive';
+      this.status = 'inactive';
     }
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,28 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    /**
+     *  Extract TailwindCSS colours as CSS variables
+     * @see (@link https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574#)
+     */
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = "") {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars =
+            typeof value === "string"
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ":root": extractColorVars(theme("colors")),
+      });
+    },
+  ],
 };
